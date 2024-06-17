c42c5c1c6755358526b24741165109bbba68abbf
luisgustavo <luisgosampaio@gmail.com>
2024-06-16 22:08:12 -0300

    asdas

---
+++ .idea/Game/Board/Board.java
@@ -0,0 +1,82 @@
+import java.util.Arraylist;
+
+public class Board {
+
+    //cartas que o jogador possui na mao
+    private Arraylist<Card> mao = new ArrayList<Card>();
+
+    //cartas no campo
+    private Arraylist<Card> infantary = new ArrayList<Card>();
+    private Arraylist<Card> artillary = new ArrayList<Card>();
+    private Arraylist<Card> siege = new ArrayList<Card>();
+
+    //pontos de cada campo
+    private int infantary_points = 0;
+    private int artillary_points = 0;
+    private int siege_points = 0;
+
+    //pontos da rodada (do jogador)
+    private int points = 0;
+
+    //vidas do jogador
+    private int hp = 2;
+
+    public tabuleiro(){
+        //construtor da mao do jogador
+        mao = new ArrayList<Card>();
+
+        //construtor dos campos de batalha
+        infantary = new ArrayList<Card>();
+        artillary = new ArrayList<Card>();
+        cerco = new ArrayList<Card>();
+    }
+
+    //getters e setters
+    public Arraylist<Card> getMao() {
+        return mao;
+    }
+
+    public void setMao(Arraylist<Card> mao) {
+        this.mao = mao;
+    }
+
+    public Arraylist<Card> getInfantary() {
+        return infantary;
+    }
+
+    public void setInfantary(Arraylist<Card> infantary) {
+        this.infantary = infantary;
+    }
+
+    public Arraylist<Card> getArtillary() {
+        return artillary;
+    }
+
+    public void setArtillary(Arraylist<Card> artillary) {
+        this.artillary = artillary;
+    }
+
+    public Arraylist<Card> getSiege() {
+        return siege;
+    }
+
+    public void setSiege(Arraylist<Card> siege) {
+        this.siege = siege;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
