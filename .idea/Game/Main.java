69a3c6cc94168262fac4c5d91d8904e8c460d867
luisgustavo <luisgosampaio@gmail.com>
2024-06-16 22:09:53 -0300

    asdas

---
+++ .idea/Game/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        public static void interface(){
+            System.out.println("1. Jogar com a Máquina");
+            System.out.println("2. Jogar contra outro jogador");
+            System.out.println("0. Sair");
+        }
+
+    }
+}
