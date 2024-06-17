1a920c25fa13ba0728f90ed8afd03bfb476f58ec
luisgustavo <luisgosampaio@gmail.com>
2024-06-17 16:01:42 -0300

    criaçao de classes player e round

--- .idea/Game/BoardAndPlayer/Player.java
+++ .idea/Game/BoardAndPlayer/Player.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Player {
 
     //vida do jogador
@@ -6,7 +8,19 @@ public class Player {
     //deck do jogador // no maximo 30 cartas!
     private Arraylist<Card> deck = new ArrayList<Card>();
 
+    //mao do jogador
+    private ArrayList<Card> hand = new ArrayList<Card>();
+
     public Player(){
         deck = new ArrayList<Card>();
+    }
+
+    public void shuffle(Arralist<Card> deck){
+        //criacao de objeto para randomizar
+        Random random = new Random();
+
+        for(int i = 0 ; i < 10; i++){
+            int randomNumber = random.nextInt();
+        }
     }
 }
